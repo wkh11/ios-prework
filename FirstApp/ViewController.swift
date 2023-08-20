f279b6910b8598e7ddc6e5943621345cd5cbac58
wkh11 <whe14@u.rochester.edu>
2023-08-20 14:27:22 -0400

    Initial Commit

---
+++ FirstApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstApp
+//
+//  Created by Wei kang He on 8/20/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
